487a0251a61726e87b003e87cfef166122c777c3
salima <salimaounoughi@gmail.com>
2023-09-05 13:09:23 +0200

    Initial Commit

---
+++ Liste Notes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Liste Notes
+//
+//  Created by Salima O. on 05/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Liste Notes/Liste_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liste_NotesApp.swift
+//  Liste Notes
+//
+//  Created by Salima O. on 05/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Liste_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
